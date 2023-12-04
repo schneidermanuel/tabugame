7057ebdcd96f84b4b3b52dc291ea78dac1be2de0
manuel <manuel@schneidermail.ch>
2023-12-04 17:08:20 +0100

    Added some boilerplate code

--- api.tabubot.brainyxs.com/src/Controllers/GameController.php
+++ api.tabubot.brainyxs.com/src/Controllers/GameController.php
@@ -2,7 +2,16 @@
 
 namespace tabubotapi\Controllers;
 
+use Schneidermanuel\Dynalinker\Controller\HttpGet;
+use tabubotapi\Core\Authenticator;
+use tabubotapi\Core\EventSource\Event;
+
 class GameController
 {
-
+    #[HttpGet("[a-zA-Z0-9]{4}/events/.*")]
+    public function MainEventStream($code, $otp)
+    {
+        $playerEntity = Authenticator::Redeem($otp);
+        Event::StartSource();
+    }
 }
